5392eda756ea836bf27ac0a8971c8a96cb870c3a
ThomasLlopiz <toomillopiz@gmail.com>
2021-12-01 17:12:27 -0300

    agregue el componente nav

---
+++ src/componentes/nav/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./nav.css"
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/index.html">Veterinaria</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarColor01">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/index.html">Mascotas</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/veterinarias.html">Veterinari@s</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/consultas.html">Consultas</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/duenos.html">Dueños</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button className="btn btn-outline-light" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+}
+export default Nav;
